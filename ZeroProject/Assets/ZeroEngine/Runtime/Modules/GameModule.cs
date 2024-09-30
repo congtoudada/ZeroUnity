131918af0fe3c14adda565ce4bbcf6ecbc38fad2
聪头的游戏世界 <1322080797@qq.com>
2024-09-30 17:31:30 +0800

    定时回调模块

--- ZeroProject/Assets/ZeroEngine/Runtime/Modules/GameModule.cs
+++ ZeroProject/Assets/ZeroEngine/Runtime/Modules/GameModule.cs
@@ -110,13 +110,13 @@ namespace ZeroEngine
         // public static SceneModule Scene => _scene ??= Get<SceneModule>();
         //
         // private static SceneModule _scene;
-        //
-        // /// <summary>
-        // /// 获取计时器模块。
-        // /// </summary>
-        // public static TimerModule Timer => _timer ??= Get<TimerModule>();
-        //
-        // private static TimerModule _timer;
+        
+        /// <summary>
+        /// 获取计时器模块。
+        /// </summary>
+        public static TimerModule Timer => _timer ??= Get<TimerModule>();
+        
+        private static TimerModule _timer;
         
         /// <summary>
         /// 资源组件拓展。
